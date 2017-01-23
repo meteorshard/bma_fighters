65b730116a133179132cb9eefac951136cdc9c30
meteorshard <doooomsday@gmail.com>
2017-01-23 15:31:36 +0800

    local test

--- localtest.py
+++ localtest.py
@@ -3,12 +3,40 @@
 
 import MySQLdb
 
-def main():
-    conn = MySQLdb.connect('localhost', 'root', 'mataleao0', 'bmagym')
-    with conn:
-        cur = conn.cursor()
-        cur.execute("CREATE TABLE IF NOT EXIST bmagym_members(Id INT PRIMARY KEY AUTO INCREASEMENT, Name VARCHAR(25))")
-        cur.execute("INSERT INTO bmagym(Name) VALUES('test')")
+db_info = {
+    'host': '127.0.0.1',
+    'user': 'root',
+    'passwd': 'mataleao0',
+    'charset': 'utf8'
+}
+
+def init_db():
+    try:
+        conn = MySQLdb.connect(**db_info)
+        with conn:
+            cur = conn.cursor()
+            db_name = 'bmagym'
+            sql = 'CREATE DATABASE IF NOT EXISTS %s' %db_name
+            cur.execute(sql)
+            conn.select_db(db_name)
+            sql = ('CREATE TABLE IF NOT EXISTS pay_log('
+                    'l_id int NOT NULL AUTO_INCREMENT,'
+                    'u_id int NOT NULL,'
+                    'pay_time datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,'
+                    'pay_amount mediumint,'
+                    'buy_duration smallint,'
+                    'event_id int,'
+                    'discount tinyint(3),'
+                    'comment text(200),'
+                    'PRIMARY KEY(l_id)'
+                   ')')
+            cur.execute(sql)
+            conn.commit()
+    except MySQLdb.Error, e:
+        print('MySQL Error [%d]: %s' %(e.args[0], e.args[1]))
+    finally:
+        cur.close()
+        conn.close()
 
 if __name__ == '__main__':
-    main()
+    init_db()
